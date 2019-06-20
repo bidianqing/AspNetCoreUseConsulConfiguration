5e8bb7229cc476d2f6544b8ebafe65bb320f01fe
毕殿卿 <bdq123@outlook.com>
2019-06-20 14:04:38 +0800

    *

--- AspNetCoreUseConsulConfiguration/Program.cs
+++ AspNetCoreUseConsulConfiguration/Program.cs
@@ -22,25 +22,6 @@ namespace AspNetCoreUseConsulConfiguration
                 {
                     var env = hostingContext.HostingEnvironment;
 
-                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-
-                    if (env.IsDevelopment())
-                    {
-                        var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
-                        if (appAssembly != null)
-                        {
-                            config.AddUserSecrets(appAssembly, optional: true);
-                        }
-                    }
-
-                    config.AddEnvironmentVariables();
-
-                    if (args != null)
-                    {
-                        config.AddCommandLine(args);
-                    }
-
                     hostingContext.Configuration = config.Build();
                     string consul_url = hostingContext.Configuration["Consul_Url"];
                     config.AddConsul(
