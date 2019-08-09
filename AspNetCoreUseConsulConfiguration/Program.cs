14837cbd97690527ade6f709bbfb167dd48e0319
毕殿卿 <bdq123@outlook.com>
2019-08-09 15:22:18 +0800

    *

--- AspNetCoreUseConsulConfiguration/Program.cs
+++ AspNetCoreUseConsulConfiguration/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using System;
-using System.Reflection;
 using System.Threading;
 using Winton.Extensions.Configuration.Consul;
 
--- AspNetCoreUseConsulConfiguration/Startup.cs
+++ AspNetCoreUseConsulConfiguration/Startup.cs
@@ -4,15 +4,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using System;
-using System.Threading;
 
 namespace AspNetCoreUseConsulConfiguration
 {
     public class Startup
     {
-        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -23,13 +19,9 @@ namespace AspNetCoreUseConsulConfiguration
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // 如果构造函数使用IOptionsSnapshot<>或IOptionsMonitor<> 则不需要下面的注入
-            // 如果构造函数使用IOptions<> 则必须添加下面的注入
-            //services.AddTransient(typeof(IOptions<>), typeof(OptionsManager<>));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.Configure<AppOptions>(Configuration.GetSection(nameof(AppOptions)));
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,33 +45,6 @@ namespace AspNetCoreUseConsulConfiguration
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-        }
-    }
-
-
-    public static class ConfigurationServiceCollectionExtensions
-    {
-        public static IServiceCollection AddConfiguration(this IServiceCollection services, IConfiguration config)
-        {
-            if (services == null)
-            {
-                throw new ArgumentNullException(nameof(services));
-            }
-
-            if (config == null)
-            {
-                throw new ArgumentNullException(nameof(config));
-            }
-
-            services.TryAddSingleton<IConfiguration>(config);
-
-            var root = config as IConfigurationRoot;
-            if (root != null)
-            {
-                services.TryAddSingleton<IConfigurationRoot>(root);
-            }
-
-            return services;
         }
     }
 }
