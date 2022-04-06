18bb834d80df7203a5ae30335c9ba314ba839ab3
bidianqing <bidianqing@longfor.com>
2022-04-06 18:00:46 +0800

    *

--- AspNetCoreUseConsulConfiguration/Program.cs
+++ AspNetCoreUseConsulConfiguration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using System;
 using Winton.Extensions.Configuration.Consul;
@@ -17,31 +18,19 @@ namespace AspNetCoreUseConsulConfiguration
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     var env = hostingContext.HostingEnvironment;
-
-                    hostingContext.Configuration = config.Build();
-                    string consul_url = hostingContext.Configuration["Consul_Url"];
-                    config.AddConsul(
-                                $"{env.ApplicationName}/appsettings.{env.EnvironmentName}.json",
-                                options =>
-                                {
-                                    options.Optional = true;
-                                    options.ReloadOnChange = true;
-                                    options.PollWaitTime = TimeSpan.FromSeconds(3);
-                                    options.OnLoadException = exceptionContext => { exceptionContext.Ignore = true; };
-                                    options.ConsulConfigurationOptions = cco => { cco.Address = new Uri(consul_url); };
-                                })
-                           .AddConsul(
-                                $"{env.ApplicationName}/test.json",
-                                options =>
-                                {
-                                    options.Optional = true;
-                                    options.ReloadOnChange = true;
-                                    options.PollWaitTime = TimeSpan.FromSeconds(3);
-                                    options.OnLoadException = exceptionContext => { exceptionContext.Ignore = true; };
-                                    options.ConsulConfigurationOptions = cco => { cco.Address = new Uri(consul_url); };
-                                });
-
-                    hostingContext.Configuration = config.Build();
+                    IConfiguration Configuration = config.Build();
+                    string consul_url = Configuration["Consul_Url"];
+                    config.AddConsul($"{env.ApplicationName}/appsettings.{env.EnvironmentName}.json", ConfigureAction)
+                          .AddConsul($"{env.ApplicationName}/test.json", ConfigureAction);
+                    
+                    void ConfigureAction(IConsulConfigurationSource options)
+                    {
+                        options.Optional = true;
+                        options.ReloadOnChange = true;
+                        options.PollWaitTime = TimeSpan.FromSeconds(3);
+                        options.OnLoadException = exceptionContext => { exceptionContext.Ignore = true; };
+                        options.ConsulConfigurationOptions = cco => { cco.Address = new Uri(consul_url); };
+                    }
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
